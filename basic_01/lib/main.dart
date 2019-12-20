58b02bbf3169eda45a0756fb2e2d2a4b1afead89
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2019-12-20 21:15:13 +0530

    AppBar and Text Widgets
    2:59:08
    Constructed a basic app with an AppBar and a Text Widget

--- basic_01/lib/main.dart
+++ basic_01/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 /*
 void main() {
   runApp(MyApp());
@@ -11,12 +10,18 @@ void main() {
 // short-hand for above commented code
 void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  // @decorators
 
-class MyApp extends StatelessWidget{
-  // @decorators 
-  
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text("Vedansh"));
+  Widget build(BuildContext context) {
+    // Scaffold creats a base page design
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Vedansh First App'),
+      ),
+      body: Text("I am currently learning"),
+    ));
   }
 }
