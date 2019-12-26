4121d3e41433168369735d1a878d27cbc33ca9dc
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2019-12-27 01:28:07 +0530

    Personality Quiz App
    Introduction to Stateful and Stateless Widgets

--- basic_01/lib/main.dart
+++ basic_01/lib/main.dart
@@ -1,27 +1,58 @@
 import 'package:flutter/material.dart';
 
-/*
-void main() {
-  runApp(MyApp());
-}
-
-*/
-
-// short-hand for above commented code
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // @decorators
 
   @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      if (questionIndex == 0) {
+        questionIndex = 1;
+      } else {
+        questionIndex = 0;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color ?",
+      "What's your favorite animal ?"
+    ];
     // Scaffold creats a base page design
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('Vedansh First App'),
       ),
-      body: Text("I am currently learning"),
+      body: Column(
+        children: <Widget>[
+          Text(questions[questionIndex]),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: answerQuestion,
+          ),
+        ],
+      ),
     ));
   }
 }
