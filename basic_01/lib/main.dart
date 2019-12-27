e4f3bd5b145cd190798f9d6fb29d6563744b543b
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2019-12-28 00:18:42 +0530

    External widget added

--- basic_01/lib/main.dart
+++ basic_01/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -7,19 +9,19 @@ class MyApp extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
   void answerQuestion() {
     setState(() {
-      if (questionIndex == 0) {
-        questionIndex = 1;
+      if (_questionIndex == 0) {
+        _questionIndex = 1;
       } else {
-        questionIndex = 0;
+        _questionIndex = 0;
       }
     });
   }
@@ -38,7 +40,7 @@ class MyAppState extends State<MyApp> {
       ),
       body: Column(
         children: <Widget>[
-          Text(questions[questionIndex]),
+          Question(questions[_questionIndex]),
           RaisedButton(
             child: Text('Answer 1'),
             onPressed: answerQuestion,
