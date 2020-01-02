17fcc0842fadd4eada96e8bbef0c9de051ea4ff4
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2020-01-03 02:06:49 +0530

    Introduction to Stacks

--- intro_layouts_container/lib/home.dart
+++ intro_layouts_container/lib/home.dart
@@ -6,43 +6,52 @@ class Home extends StatelessWidget{
     return Container(
       color: Colors.greenAccent,
       
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+
+      child: new Stack(
+        alignment: Alignment.bottomCenter,
         children: <Widget>[
-          new Text("Item 1 ", style: new TextStyle(color: Colors.white, fontSize: 18, ),),
-          new Container(
-            color: Colors.deepOrange,
-            child: new Text("Item 3", style: new TextStyle(color: Colors.black, fontSize: 18),),
-          )
+          const Text("Hello There"),
+          const Text("Hey Again"),
+          const Text("1")
         ],
       ),
-      
-      
-      
-      
-      
-      
-      /*
-        child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new Text("Item 1", style: new TextStyle(color: Colors.white),),
-          new Text("Item 2", style: new TextStyle(color: Colors.blue),),
-          new Container(
-            color: Colors.deepOrange,
-            alignment: Alignment.bottomRight,
-            child: new Text("Item 3", style: new TextStyle(color: Colors.black),),
-          ),
-        ],
-      ),*/
 
 
 
-      /*child: Text(
-        "Hello Container",
-        textDirection: TextDirection.ltr,
-        style: new TextStyle(color: Colors.white, fontWeight: FontWeight.w900, fontSize: 18.2),
-      ), */
+      
+      // child: new Row(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: <Widget>[
+      //     new Text("Item 1 ", style: new TextStyle(color: Colors.white, fontSize: 18, ),),
+      //     new Container(
+      //       color: Colors.deepOrange,
+      //       child: new Text("Item 3", style: new TextStyle(color: Colors.black, fontSize: 18),),
+      //     )
+      //   ],
+      // ),
+      
+      
+      
+      //   child: new Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: <Widget>[
+      //     new Text("Item 1", style: new TextStyle(color: Colors.white),),
+      //     new Text("Item 2", style: new TextStyle(color: Colors.blue),),
+      //     new Container(
+      //       color: Colors.deepOrange,
+      //       alignment: Alignment.bottomRight,
+      //       child: new Text("Item 3", style: new TextStyle(color: Colors.black),),
+      //     ),
+      //   ],
+      // ),
+
+
+
+      // child: Text(
+      //   "Hello Container",
+      //   textDirection: TextDirection.ltr,
+      //   style: new TextStyle(color: Colors.white, fontWeight: FontWeight.w900, fontSize: 18.2),
+      // ), 
       alignment: Alignment.center,
     );
   }
