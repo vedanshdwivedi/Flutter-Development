42658aedc65a16528756acbdabe65607f819444d
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2020-01-03 19:59:55 +0530

    Introduction to Inkwell

--- intro_scaffolds/lib/Scaffold.dart
+++ intro_scaffolds/lib/Scaffold.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
-
-   _onPress(){
+class Home extends StatelessWidget {
+  _onPress() {
     print("Search Tapped");
   }
 
@@ -12,11 +11,37 @@ class Home extends StatelessWidget{
       appBar: new AppBar(
         backgroundColor: Colors.greenAccent.shade700,
         title: new Text("Scaffold App Bar"),
-
         actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.send), onPressed: () => debugPrint('Send Tapped'),),
-          new IconButton(icon: new Icon(Icons.search), onPressed: _onPress,),
+          new IconButton(
+            icon: new Icon(Icons.send),
+            onPressed: () => debugPrint('Send Tapped'),
+          ),
+          new IconButton(
+            icon: new Icon(Icons.search),
+            onPressed: _onPress,
+          ),
         ],
+      ),
+
+      // Other Properties
+      backgroundColor: Colors.grey[300],
+      body: new Container(
+        alignment: Alignment.center,
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Hello Vedansh",
+                style: new TextStyle(
+                    fontWeight: FontWeight.w400,
+                    color: Colors.deepOrange,
+                    fontSize: 14.5)),
+
+            new InkWell(
+              child: new Text("Tap Me"),
+              onTap: () => debugPrint('Inkwell Tapped'),
+            ),
+          ],
+        ),
       ),
     );
   }
