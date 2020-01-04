cb77b87003ba2e0e7064df2482599d30681c3660
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2020-01-05 02:33:17 +0530

    Setting up files for login form app

---
+++ login_app/lib/loginapp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CustomLoginForm extends StatefulWidget {
+  @override
+  _CustomLoginFormState createState() => _CustomLoginFormState();
+}
+
+class _CustomLoginFormState extends State<CustomLoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ login_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './loginapp.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    title: 'Login App',
+    home: new CustomloginForm(),
+  ));
+}
