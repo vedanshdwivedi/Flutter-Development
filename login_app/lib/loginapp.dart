075f94e875df4af84bb9131fd39b6927167d62a4
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2020-01-05 23:58:37 +0530

    UI for Login Form App

--- login_app/lib/loginapp.dart
+++ login_app/lib/loginapp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class CustomLoginForm extends StatefulWidget {
   @override
@@ -6,8 +7,85 @@ class CustomLoginForm extends StatefulWidget {
 }
 
 class _CustomLoginFormState extends State<CustomLoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Text("Hello Vedansh");
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // add image
+          Center(
+            child: Image.asset(
+              "images/face.png",
+              width: 90,
+              height: 90,
+              color: Colors.deepOrange
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Name",
+                border: OutlineInputBorder(
+                  gapPadding: 3.3,
+                  borderRadius: BorderRadius.circular(3.3)
+                )
+              ),
+            ),
+          ),
+
+
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(
+                  gapPadding: 3.3,
+                  borderRadius: BorderRadius.circular(3.3)
+                )
+              ),
+            ),
+          ),
+
+
+          // add buttons
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                // Add the actual buttons
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    onPressed: (){
+
+                    },
+                    child: Text("Submit"),
+                  ),
+                ),
+
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    onPressed: (){
+
+                    },
+                    child: Text("Clear"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
