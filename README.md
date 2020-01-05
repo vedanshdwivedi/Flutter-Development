518686d9764161860f277f32ab8f2d37ec32b914
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2020-01-06 01:18:59 +0530

    Login Form App Completed

---
+++ README.md
@@ -0,0 +1,8 @@
+# Projects Covered
+
+[+] Login Form App
+[+] Weight on Planet X
+[+] BMI App
+[+] Quake App
+[+] Weather App
+[+] No-Todo App
--- login_app/lib/loginapp.dart
+++ login_app/lib/loginapp.dart
@@ -6,8 +6,14 @@ class CustomLoginForm extends StatefulWidget {
   _CustomLoginFormState createState() => _CustomLoginFormState();
 }
 
+class _LoginData {
+  String name = "";
+  String password = "";
+}
+
 class _CustomLoginFormState extends State<CustomLoginForm> {
   final _formKey = GlobalKey<FormState>();
+  _LoginData _data = new _LoginData();
 
   @override
   Widget build(BuildContext context) {
@@ -18,42 +24,54 @@ class _CustomLoginFormState extends State<CustomLoginForm> {
         children: <Widget>[
           // add image
           Center(
-            child: Image.asset(
-              "images/face.png",
-              width: 90,
-              height: 90,
-              color: Colors.deepOrange
-            ),
+            child: Image.asset("images/face.png",
+                width: 90, height: 90, color: Colors.deepOrange),
           ),
 
+          // Name Field
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextFormField(
               decoration: InputDecoration(
-                labelText: "Name",
-                border: OutlineInputBorder(
-                  gapPadding: 3.3,
-                  borderRadius: BorderRadius.circular(3.3)
-                )
-              ),
+                  labelText: "Name",
+                  border: OutlineInputBorder(
+                      gapPadding: 3.3,
+                      borderRadius: BorderRadius.circular(3.3))),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "Please Enter Name";
+                } else {
+                  // show something
+                  setState(() {
+                    _data.name = value;
+                  });
+                  print("Data: ${_data.name}");
+                }
+              },
             ),
           ),
 
-
+          // Password Field
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextFormField(
               obscureText: true,
               decoration: InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(
-                  gapPadding: 3.3,
-                  borderRadius: BorderRadius.circular(3.3)
-                )
-              ),
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                      gapPadding: 3.3,
+                      borderRadius: BorderRadius.circular(3.3))),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "Please Enter Password";
+                } else {
+                  setState(() {
+                    _data.password = value;
+                  });
+                }
+              },
             ),
           ),
-
 
           // add buttons
           Container(
@@ -61,28 +79,53 @@ class _CustomLoginFormState extends State<CustomLoginForm> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 // Add the actual buttons
+
+                // Submit Button
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: RaisedButton(
-                    onPressed: (){
-
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text("All is good.")));
+                      }
                     },
                     child: Text("Submit"),
                   ),
                 ),
 
-
+                // Clear Button
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: RaisedButton(
-                    onPressed: (){
-
+                    onPressed: () {
+                      if (_formKey.currentState != null) {
+                        // clear everything
+                        _formKey.currentState.reset();
+                        setState(() {
+                          _data.name="";
+                          _data.password="";
+                        });
+                      }
                     },
                     child: Text("Clear"),
                   ),
                 ),
               ],
             ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Center(
+              child: 
+                _data.name.isEmpty ? Text("") : Text(
+              "Welcome ${_data.name}",
+              style: new TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19.0,
+                  color: Colors.deepOrange),
+            )),
           ),
         ],
       ),
