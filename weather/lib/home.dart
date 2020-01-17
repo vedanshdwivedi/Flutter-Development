6189cbd6f92d4145af7207c45c24c69ddeb830d5
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2020-01-17 17:48:00 +0530

    Create a Weather App

---
+++ weather/lib/home.dart
@@ -0,0 +1,26 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+
+// unsplash.com : for free images
+// tinypng.com  : to minimise images
+
+class Klimatic extends StatefulWidget {
+  @override
+  _KlimaticState createState() => _KlimaticState();
+}
+
+class _KlimaticState extends State<Klimatic> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Klimatic"),
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
